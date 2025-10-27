3c0d91cc948a1af91b2e3e8f0acf3166c8951a47
Krestine H <krha0001@stud.kea.dk>
2025-10-27 13:07:39 +0100

    everything is done

--- js/api-consumption.js
+++ js/api-consumption.js
@@ -6,7 +6,9 @@ apiFetch(firstFetch);
 
 document.querySelectorAll("button").forEach(menuOption => {
     menuOption.addEventListener("click", function() {
-        const dataTarget = this.getAttribute("data-target")
+        document.querySelector(".selected").removeAttribute("class", "selected");
+        this.setAttribute("class", "selected");
+        const dataTarget = this.getAttribute("data-target");
         apiFetch(dataTarget);
     })
 })
@@ -20,7 +22,7 @@ function apiFetch (choosenEndPoint){fetch(`${api}${choosenEndPoint}?language=en-
     data.results.forEach(movie => {
         const newMovie = document.querySelector("#card-template").content.cloneNode(true);
         newMovie.querySelector(".title").innerText = movie.title;
-        newMovie.querySelector("img").setAttribute("src", `${movie.poster_path}`);
+        newMovie.querySelector("img").setAttribute("src", `https://image.tmdb.org/t/p/w500${movie.poster_path}`);
         newMovie.querySelector("img").setAttribute("alt", `${movie.title}`);
         newMovie.querySelector(".movie-overview").innerText = movie.overview;
         newMovie.querySelector(".original-title").innerText = movie.original_title;
